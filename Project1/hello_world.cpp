fd32fa35ce854e800871657517051845d4860c05
Minjae Lee <lmgdlekd@naver.com>
2025-01-15 17:22:52 +0900

    초기 커밋.

---
+++ Project1/hello_world.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+inline void keep_window_open() { char ch; cin >> ch; }//일부 window system에서 창이 닫히는 일을 방지하는 용도.
+
+int main()
+{
+	cout << "Hello, programing! \n";
+	cout << "Here we go! \n";
+	keep_window_open(); //문자 입력을 기다림.
+	return 0;
+}
+
